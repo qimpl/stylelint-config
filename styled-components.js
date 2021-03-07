bb1a503f60d25329a2acd5bc165d11d5ce56a089
Jérôme Pogeant <p-jerome@hotmail.fr>
2021-03-07 10:40:25 +0100

    feat(config): improve styled components config

--- styled-components.js
+++ styled-components.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['./index.js', 'stylelint-config-styled-components'],
   processors: ['stylelint-processor-styled-components'],
+  rules: {
+    'value-keyword-case': null,
+  },
 };
